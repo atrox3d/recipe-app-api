dfa787ca3647fade04188bd51543a968397fe8bd
atrox3d <atrox3d@gmail.com>
2021-06-28 14:39:33 +0200

    add path to messages

--- app/examples/tests/test.py
+++ app/examples/tests/test.py
@@ -3,29 +3,31 @@ import sys
 #
 #   detect the right test framework analyzing the command line
 #
-print(f"INFO| detecting test framework from sys.argv[0]({sys.argv[0]})")
+print(f"{__file__} | INFO |  "
+      f"detecting test framework from sys.argv[0]({sys.argv[0]})")
 if 'manage.py' in sys.argv[0]:
     from django.test import TestCase
 elif "-m unittest" in sys.argv[0]:
     from unittest import TestCase
 else:
     raise SystemExit("unable to detect test framework")
-print(f"INFO| detected: {TestCase.__module__}.{TestCase.__qualname__}")
+print(f"{__file__} | INFO |  "
+      f"detected: {TestCase.__module__}.{TestCase.__qualname__}")
 
 
 #
 #   import module to be tested with the right path
 #
 try:
-    print("TRY | from app.calc import add")
+    print(f"{__file__} | TRY  | from app.calc import add")
     from examples.calc import add
 except ModuleNotFoundError as e:
-    print("FAIL| " + repr(e))
+    print(f"{__file__} | FAIL| " + repr(e))
     try:
         print("using : from app.app.calc import add")
         from app.examples.calc import add
     except ModuleNotFoundError as e:
-        print("FAIL| " + repr(e))
+        print(f"{__file__} | FAIL| " + repr(e))
         raise SystemExit("cannot import calc")
 
 
