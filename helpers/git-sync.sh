a686f9583314249a95d7abd244b2d78f9277d432
atrox3d <atrox3d@gmail.com>
2021-06-28 13:51:46 +0200

    rename private paths variables

--- helpers/git-sync.sh
+++ helpers/git-sync.sh
@@ -3,9 +3,9 @@ HELPERS_DIR="$(dirname ${BASH_SOURCE[0]})"
 . "${HELPERS_DIR}/.private.env"
 
 echo "you are here: $(pwd)"
-echo "cd ${NB_PATH}"
+echo "cd ${NOTEBOOK_CODE_PATH}"
 
-cd "${NB_PATH}"
+cd "${NOTEBOOK_CODE_PATH}"
 echo "you are here: $(pwd)"
 
 git pull
