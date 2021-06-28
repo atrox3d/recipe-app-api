27a2c1389698ed9a08e1de9e9c132320a0f6b43e
atrox3d <atrox3d@gmail.com>
2021-06-28 15:12:55 +0200

    update helper: add multiple test paths

--- helpers/docker-test.sh
+++ helpers/docker-test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-echo docker-compose run app sh -c "python manage.py test examples/ && echo \"flake8:\" && flake8 --count"
-docker-compose run app sh -c "python manage.py test examples/ && echo \"flake8:\" && flake8 --count"
+ALL_TEST_DIRS=(examples/ core/)
+
+if [ $# -ge 1 ]
+then
+    TEST_DIRS="${@}"
+else
+    TEST_DIRS="${ALL_TEST_DIRS[@]}"
+fi
+
+echo "TEST_DIRS: ${TEST_DIRS[@]}"
+echo docker-compose run app sh -c "python manage.py ${TEST_DIRS} && echo \"flake8:\" && flake8 --count"
+docker-compose run app sh -c "python manage.py test ${TEST_DIRS} && echo \"flake8:\" && flake8 --count"
