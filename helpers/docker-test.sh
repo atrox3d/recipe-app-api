6084dcf425619064905f593ee9953eec93831b99
atrox3d <atrox3d@gmail.com>
2021-06-28 19:56:17 +0200

    output formatted test dir list

--- helpers/docker-test.sh
+++ helpers/docker-test.sh
@@ -3,11 +3,12 @@ ALL_TEST_DIRS=(examples/ core/)
 
 if [ $# -ge 1 ]
 then
-    TEST_DIRS="${@}"
+    TEST_DIRS=("${@}")
 else
-    TEST_DIRS="${ALL_TEST_DIRS[@]}"
+    TEST_DIRS=("${ALL_TEST_DIRS[@]}")
 fi
-
-echo "TEST_DIRS: ${TEST_DIRS[@]}"
+DIR_LIST="[ $(IFS=, ;echo "${TEST_DIRS[*]}") ]"
+DIR_LIST="${DIR_LIST//,/, }"
+echo "TEST_DIRS: $DIR_LIST"
 echo docker-compose run app sh -c "python manage.py ${TEST_DIRS} && echo \"flake8:\" && flake8 --count"
 docker-compose run app sh -c "python manage.py test ${TEST_DIRS} && echo \"flake8:\" && flake8 --count"
