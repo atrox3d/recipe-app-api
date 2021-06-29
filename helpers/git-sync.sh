143b360f11dfbde5e0b6334712346c70f34b1da6
atrox3d <atrox3d@gmail.com>
2021-06-29 08:53:34 +0200

    better output

--- helpers/git-sync.sh
+++ helpers/git-sync.sh
@@ -2,10 +2,8 @@
 HELPERS_DIR="$(dirname ${BASH_SOURCE[0]})"
 . "${HELPERS_DIR}/.private.env"
 
-echo "you are here: $(pwd)"
-echo "cd ${NOTEBOOK_CODE_PATH}"
-
+echo "current path --> $(pwd)"
+echo "move to      --> ${NOTEBOOK_CODE_PATH}"
 cd "${NOTEBOOK_CODE_PATH}"
-echo "you are here: $(pwd)"
-
+echo "current path --> $(pwd)"
 git pull
