0ed0c626a9b172ff9d5eb2749bd7b6e7fabffc1d
atrox3d <atrox3d@gmail.com>
2021-06-29 16:36:33 +0200

    add getopts

--- helpers/docker-test.sh
+++ helpers/docker-test.sh
@@ -1,14 +1,81 @@
 #!/bin/bash
+
 ALL_TEST_DIRS=(examples core app)
+DEFAULT_DIRS=(core app)
+TEST_DIRS=("${DEFAULT_DIRS[@]}")
+VERBOSE=""
+FLAKE8=false
+
+SYNTAX=(
+    "${BASH_SOURCE[0]} [ -a -d -D \"dir1 dir2 ...\" -f -v LEVEL  ] [ dir1 dir2 ... ]"
+    "-a: all test dirs"
+    "-d: default test dirs"
+    "-D: use \"dir1 dir2 ...\""
+    "-f: enable flake8"
+    "-v: set verbose level (0-3)"
+    "directories to test"
+)
+
+
+while getopts ":afD:dv:" opt
+do
+		case $opt in
+			a)
+                TEST_DIRS=("${ALL_TEST_DIRS[@]}")
+			;;
+			d)
+                TEST_DIRS=("${DEFAULT_DIRS[@]}")
+			;;
+			D)
+				read -r -a TEST_DIRS <<< "$OPTARG"
+			;;
+			f)
+				FLAKE8=true
+			;;
+            h)
+				echo "syntax:"
+                for line in "${SYNTAX[@]}"
+                do
+                    echo "$line"
+                done
+                exit 0
+            ;;
+            v)
+                VERBOSE="-v${OPTARG}"
+            ;;
+			\?)
+				echo "ERROR | unknown parameter -${OPTARG}"
+				echo "syntax:"
+                for line in "${SYNTAX[@]}"
+                do
+                    echo "$line"
+                done
+				exit 1
+			;;
+			:)
+				echo "ERROR | missing value for parameter -${OPTARG }"
+				exit 2
+			;;
+		esac
+done
+shift "$((OPTIND-1))"
 
 if [ $# -ge 1 ]
 then
     TEST_DIRS=("${@}")
-else
-    TEST_DIRS=("${ALL_TEST_DIRS[@]}")
 fi
+
 DIR_LIST="[ $(IFS=, ;echo "${TEST_DIRS[*]}") ]"
 DIR_LIST="${DIR_LIST//,/, }"
 echo "TEST_DIRS: $DIR_LIST"
-echo docker-compose run app sh -c "python manage.py test ${TEST_DIRS[*]};echo \"flake8:\" && flake8 --count"
-docker-compose run app sh -c "python manage.py test ${TEST_DIRS[*]};echo \"flake8:\" && flake8 --count"
+echo "FLAKE8   : $FLAKE8"
+echo "ARGS     : ${*}"
+
+
+[ "${FLAKE8}" == "true" ] && {
+    echo docker-compose run app sh -c "python manage.py test ${VERBOSE} ${TEST_DIRS[*]};echo \"flake8:\"; flake8 --count"
+    docker-compose run app sh -c "python manage.py test ${VERBOSE} ${TEST_DIRS[*]};echo \"flake8:\"; flake8 --count"
+} || {
+    echo docker-compose run app sh -c "python manage.py test ${VERBOSE} ${TEST_DIRS[*]}"
+    docker-compose run app sh -c "python manage.py test ${VERBOSE} ${TEST_DIRS[*]}"
+}
