4c19942942acf9ab358fb01c8d32870c41274651
atrox3d <atrox3d@gmail.com>
2021-06-30 19:42:51 +0200

    list custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+from utils import testing
+
+TestCase = testing.detect_testcase_framework()
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@londonappdev.com',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@londonappdev.com',
+            password='password123',
+            name='Test User Full Name',
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on the user page"""
+
+        url = reverse('admin:core_user_changelist')
+        print(f"INFO| url={url}")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
